b98ebde1f01a65759a624510bf7ee26a13ad2af9
rafiqwe <rmlmanik502@gmail.com>
2025-08-13 21:11:00 +0600

    Created login & logout routes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const userController = require("../controllers/user.controller");
-
+const authMiddleware = require("../middleware/authMiddleware");
 router.post(
   "/register",
   [
@@ -17,5 +17,17 @@ router.post(
   userController.registerUser
 );
 
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  userController.loginUser
+);
 
-module.exports = router
+router.get("/logout", authMiddleware.auth, userController.logoutUser);
+
+module.exports = router;
