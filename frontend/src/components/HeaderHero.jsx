720a74a4e17d958df068c2cc2d20a2eaa32f5481
rafiqwe <rmlmanik502@gmail.com>
2025-08-16 22:49:15 +0600

    Created header hero component

---
+++ frontend/src/components/HeaderHero.jsx
@@ -0,0 +1,11 @@
+import DarkVeil from "./DarkVail";
+
+const HeaderHero = () => {
+  return (
+    <div style={{ width: "100%", height: "100vh", position: "relative" }}>
+      <DarkVeil />
+    </div>
+  );
+};
+
+export default HeaderHero;
