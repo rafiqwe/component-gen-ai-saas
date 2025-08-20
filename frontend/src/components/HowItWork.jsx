7cb7b20decacd918d3742ae56c841d316875c4ee
rafiqwe <rmlmanik502@gmail.com>
2025-08-20 08:39:02 +0600

    Add animation using gsap

--- frontend/src/components/HowItWork.jsx
+++ frontend/src/components/HowItWork.jsx
@@ -1,6 +1,12 @@
 import { FileCode2, Play, Download } from "lucide-react";
 import { motion } from "framer-motion";
 import GlowLine from "./GlowLine";
+import SpotlightCard from "./SportLightCard";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const steps = [
   {
@@ -24,33 +30,98 @@ const steps = [
 ];
 
 export default function HowItWorks() {
+  const cardsRef = useRef([]);
+  const titleRef = useRef(null);
+  const subtitleRef = useRef(null);
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.fromTo(
+      titleRef.current,
+      { opacity: 0, scale: 0.5 },
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 0.8,
+        stagger: 0.2,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: titleRef.current, // trigger when first card enters viewport
+          scroller: "body",
+          start: "top 100%",
+          end: "top 50%",
+          scrub: 0.5,
+        },
+      }
+    );
+    tl.fromTo(
+      subtitleRef.current,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        duration: 0.8,
+        stagger: 0.2,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: subtitleRef.current, // trigger when first card enters viewport
+          scroller: "body",
+          start: "top 100%",
+          end: "top 50%",
+          scrub: 0.5,
+        },
+      }
+    );
+    tl.fromTo(
+      cardsRef.current,
+      { opacity: 0, scale: 0.5 },
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 0.8,
+        stagger: 0.2,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: cardsRef.current[0], // trigger when first card enters viewport
+          scroller: "body",
+          start: "top 80%", // when card is 80% down viewport
+          end: "top 50%",
+          scrub: 0.6,
+        },
+      }
+    );
+  });
+
   return (
-    <section className="relative py-15 md:20 mt-5 md:mt-0  h-full  md:h-screen flex items-center ">
+    <section id="howitwork" className="relative py-15 md:20 mt-5 md:mt-0  h-full  md:h-screen flex items-center ">
       <GlowLine orientation="horizontal" position="0%" color="red" />
       <div className="max-w-6xl mx-auto text-center">
-        <h2 className="text-4xl md:text-5xl font-bold text-white mb-6">
+        <h2
+          ref={titleRef}
+          className="text-4xl md:text-5xl font-bold text-white mb-6"
+        >
           How It Works
         </h2>
-        <p className="text-gray-300 text-lg max-w-2xl mx-auto mb-16">
+        <p
+          ref={subtitleRef}
+          className="text-gray-300 text-lg max-w-2xl mx-auto mb-16"
+        >
           From idea to production in just three simple steps.
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-12 relative">
           {steps.map((step, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0, y: 40 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: index * 0.2 }}
-              viewport={{ once: true }}
-              className="p-8 rounded-3xl backdrop-blur-lg bg-white/10 border border-white/20 shadow-xl flex flex-col items-center text-center"
-            >
-              <div className="mb-6">{step.icon}</div>
-              <h3 className="text-xl font-semibold text-white mb-3">
-                {step.title}
-              </h3>
-              <p className="text-gray-300 text-sm">{step.description}</p>
-            </motion.div>
+            <SpotlightCard key={index}>
+              <div
+                ref={(el) => (cardsRef.current[index] = el)}
+                className="p-8 rounded-3xl backdrop-blur-lg bg-white/10 border border-white/20 shadow-xl flex flex-col items-center text-center"
+              >
+                <div className="mb-6">{step.icon}</div>
+                <h3 className="text-xl font-semibold text-white mb-3">
+                  {step.title}
+                </h3>
+                <p className="text-gray-300 text-sm">{step.description}</p>
+              </div>
+            </SpotlightCard>
           ))}
         </div>
       </div>
