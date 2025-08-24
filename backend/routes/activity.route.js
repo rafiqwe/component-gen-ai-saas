a4202a392f19ef4a40ad1c9af2816fbff4e76770
rafiqwe <rmlmanik502@gmail.com>
2025-08-24 22:43:49 +0600

    Created activity, usage route and update user route

---
+++ backend/routes/activity.route.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const activityModel = require("../models/activity.model");
+const authMeddleware = require("../middleware/authMiddleware");
+
+router.get("/activity", authMeddleware.auth, async (req, res) => {
+  //   const { userId } = req.query;
+  const userId = req.user.id;
+  const activities = await activityModel
+    .find({ userId })
+    .sort({ createdAt: -1 })
+    .limit(5);
+  console.log(activities);
+
+  res.status(200).json(activities);
+});
+
+module.exports = router;
