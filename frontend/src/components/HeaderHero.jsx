3f4c373533d84a6cd647dc91f00487294a34c162
rafiqwe <rmlmanik502@gmail.com>
2025-09-11 12:12:56 +0600

    Add header texts

--- frontend/src/components/HeaderHero.jsx
+++ frontend/src/components/HeaderHero.jsx
@@ -1,9 +1,15 @@
 import DarkVeil from "./DarkVail";
+import HeroText from "./HeroText";
 
 const HeaderHero = () => {
   return (
-    <div style={{ width: "100%", height: "100vh", position: "relative" }}>
-      <DarkVeil />
+    <div className="w-full ">
+      <div style={{ width: "100%", height: "100vh", position: "relative" }}>
+        <DarkVeil />
+      </div>
+      <div className="absolute top-50 px-3 z-10  text-white flex items-center justify-center w-[99%] md:w-full" >
+        <HeroText/>
+      </div>
     </div>
   );
 };
